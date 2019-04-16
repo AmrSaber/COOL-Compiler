9d0b5cdfd5d2f8bc7c848be2bf0ae47426c796c1
muhammad-magdi <muhammad.magdi1024@gmail.com>
2019-04-17 01:30:28 +0200

    Added a separated Lexer Class

---
+++ src/Lexer.java
@@ -0,0 +1,78 @@
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lexer {
+    private CoolLexer lexer;
+    private List<Token> tokens;
+    private List<Token> errors;
+    private List<String> outTokens;
+
+    public Lexer(String fileName)throws IOException {
+        CharStream charStream = CharStreams.fromFileName(fileName);
+        lexer = new CoolLexer(charStream);
+        CommonTokenStream tokensStream = new CommonTokenStream(lexer);
+        tokensStream.fill();
+        tokens = tokensStream.getTokens();
+        lex();
+    }
+
+    public List<String> getTokens(){
+        return outTokens;
+    }
+
+    public List<String> getTokens(String outputFile) throws IOException{
+        BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile));
+        for(String token : outTokens){
+            writer.write(token + "\n");
+        }
+        writer.close();
+        return outTokens;
+    }
+
+    public boolean hasErrors(){return !errors.isEmpty();}
+
+    public void reportErrors(){
+        for(Token errorToken : errors){
+            System.out.println("Lexer ERROR: Line "
+                    + errorToken.getLine()
+                    + " - invalid character "
+                    + errorToken.getText());
+        }
+    }
+
+    private void lex(){
+        outTokens = new ArrayList<>();
+        errors = new ArrayList<>();
+        String [] ruleNames = lexer.getRuleNames();
+        for(Token token : tokens){
+            if(token.getType() > 0){        //Not EOF
+                if(lexer.getRuleNames()[token.getType()-1].equals("ErrorChar")){
+                    errors.add(token);
+                }else{
+                    outTokens.add(Integer.toString(token.getLine()));
+                    outTokens.add(ruleNames[token.getType()-1]);
+                    if(shouldBeTriplet(ruleNames[token.getType()]))
+                        outTokens.add(token.getText());
+                }
+            }
+        }
+    }
+
+    private boolean shouldBeTriplet(String  tokenName){
+        if(tokenName.equals("SELF")
+          || tokenName.equals("STRING")
+          || tokenName.equals("NUM")
+          || tokenName.equals("LITERAL")
+          || tokenName.equals("ID"))
+            return true;
+        return false;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,13 @@
-
-import org.antlr.v4.runtime.*;
-
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) throws Exception{
-        String srcFile = "cool_source/bad.cl";
+        String srcFile = "cool_source/good.cl";
         if(args.length >= 1)    srcFile = args[0];
 
-        String inputFileName = srcFile;
+        Lexer lexer = new Lexer(srcFile);
 
-        CharStream input = CharStreams.fromFileName(inputFileName);
-        CoolLexer lexer = new CoolLexer(input);
-
-        CommonTokenStream tokensStream = new CommonTokenStream(lexer);
-        tokensStream.fill();
-
-        List<Token> tokens = tokensStream.getTokens();
-
-        if(isGoodCode(tokens)){
-            BufferedWriter writer = new BufferedWriter(new FileWriter(inputFileName+"-lex"));
-            for(Token token: tokens){
-                if(token.getType() > 0){
-                    writer.write(token.getLine() + "\n");
-                    writer.write(lexer.getRuleNames()[token.getType() - 1].toLowerCase() + "\n");
-                    if (shouldBeTriplet(token.getType()))
-                        writer.write(token.getText() + "\n");
-                }
-            }
-            writer.close();
-        }else{
-            for(Token token: tokens){
-                if(token.getType() > 0 && isError(token.getType()))
-                    System.out.println("ERROR : " + token.getLine() + ": Lexer: invalid character " + token.getText());
-            }
-        }
-
+        if(lexer.hasErrors())   lexer.reportErrors();
+        else    lexer.getTokens(srcFile + "-lex");
 
         /*
         CoolParser parser = new CoolParser(tokensStream);
@@ -46,25 +15,5 @@ public class Main {
 
         System.out.println(tree.toStringTree(parser));
         */
-    }
-
-    public static boolean shouldBeTriplet(int typeID){
-        switch (typeID){
-            case 50: case 6 : case 7 : case 48 : case 9 :
-                return true;
-            default: return false;
-        }
-    }
-
-    public static boolean isError(int typeID){
-        return typeID == 52;
-    }
-
-    public static boolean isGoodCode(List<Token> tokens){
-        for(Token token : tokens){
-            if(isError(token.getType()))
-                return false;
-        }
-        return true;
     }
 }
