1d196d12174c5d6bff6a6a542685ee01666a4aef
Amr Saber <amr.m.saber.mail@gmail.com>
2019-05-04 23:18:19 +0200

    Implement TermTranslator

--- src/translation/translators/TermTranslator.java
+++ src/translation/translators/TermTranslator.java
@@ -1,16 +1,44 @@
 package translation.translators;
 
+import gen.CoolParser;
 import org.antlr.v4.runtime.tree.ParseTree;
 import translation.Temp;
+import translation.TranslationHandler;
 import translation.Translator;
 
+// term op=(OP_MUL|OP_DIV) value | value;
 public class TermTranslator extends Translator {
     public TermTranslator(ParseTree parseTree) {
         super(parseTree);
+        if (!(parseTree instanceof CoolParser.TermContext))
+            throw new RuntimeException("Node is not Term!");
     }
 
     @Override
     public Temp generate() {
-        return new Temp();
+
+        // if the term consists of one value
+        if (parseTree.getChildCount() == 1) {
+            return new ValueTranslator(parseTree.getChild(0)).generate();
+        }
+
+        Temp termTemp = new TermTranslator(parseTree.getChild(0)).generate();
+        Temp valueTemp = new ValueTranslator(parseTree.getChild(2)).generate();
+        Temp myRes = new Temp();
+
+        TranslationHandler.write(
+                String.format(
+                        "%s := %s %s %s\t",
+                        myRes,
+                        termTemp,
+                        parseTree.getChild(1).getText(),
+                        valueTemp
+                )
+        );
+
+        valueTemp.release();
+        termTemp.release();
+
+        return myRes;
     }
 }
